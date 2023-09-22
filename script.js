5b284efc7ff87f4a71b726d5788413d4d21531ef
Alexandro Jorge <alexandro.jorge@proway.treina>
2023-09-22 13:48:56 -0300

    colocando botao editar

--- script.js
+++ script.js
@@ -29,6 +29,11 @@ function enviar_dados() {
 }
 
 function atualizarTabela() {
+
+    document.getElementById("nome-input").value = "";
+    document.getElementById("email-input").value = "";
+    document.getElementById("numero-input").value = "";
+
     var tabela = document.getElementById("minha-tabela");
     var tbody = tabela.querySelector("tbody");
 
@@ -46,6 +51,8 @@ function atualizarTabela() {
         }
 
         var colunaAcoes = document.createElement("td");
+
+        //botao excluir
         var botaoExcluir = document.createElement("button");
         botaoExcluir.textContent = "Excluir";
         botaoExcluir.classList.add("excluir");
@@ -55,7 +62,19 @@ function atualizarTabela() {
             excluir_linha(this);
         };
 
+        //botao editar
+        var botaoEditar = document.createElement("button");
+        botaoEditar.textContent = "Editar";
+        botaoEditar.classList.add("editar");
+        botaoEditar.dataset.index = i;
+
+        botaoEditar.onclick = function () {
+            editar_linha(this);
+        };
+
+
         colunaAcoes.appendChild(botaoExcluir);
+        colunaAcoes.appendChild(botaoEditar);
         linha.appendChild(colunaAcoes);
 
         tbody.appendChild(linha);
@@ -69,7 +88,47 @@ function excluir_linha(botao) {
         atualizarTabela();
     }
 }
+function editar_linha(botao2) {
+    var index = botao2.dataset.index;
+    objeto = dados[index]
+    document.getElementById("nome-input").value = objeto.nome;
+    document.getElementById("email-input").value = objeto.email;
+    document.getElementById("numero-input").value = objeto.numero;
+    //texto Editar 
+    var titulo = document.querySelector("#titulo")
+    titulo.textContent = "Editar"
+    //botao atualizar
+    var botaoAtualizar = document.querySelector("#botaoGravar")
+    botaoAtualizar.textContent = "Atualizar"
+    botaoAtualizar.onclick = function () {
+        alterarCadastro(botao2);
+    }
+}
+function alterarCadastro(botao2) {
+    var index = botao2.dataset.index;
+    if (index !== undefined) {
+        var objeto = dados[index];
 
+        var nomeAlterado = document.getElementById("nome-input").value;
+        var emailAlterado = document.getElementById("email-input").value;
+        var numeroAlterado = document.getElementById("numero-input").value;
+
+        objeto.nome = nomeAlterado;
+        objeto.email = emailAlterado;
+        objeto.numero = numeroAlterado;
+
+        var titulo = document.querySelector("#titulo")
+        titulo.textContent = "Cadastro"
+
+        var botaoAtualizar = document.querySelector("#botaoGravar")
+        botaoAtualizar.textContent = "Gravar"
+        botaoAtualizar.onclick = function () {
+            enviar_dados();
+        }
+        atualizarTabela();
+    }
+}
+// não deixa a pagina reiniciar após clicar no button
 var forms = document.getElementById("forms");
 
 forms.addEventListener("submit", function (event) {
@@ -77,3 +136,10 @@ forms.addEventListener("submit", function (event) {
 });
 
 atualizarTabela();
+
+var a = 2
+var a2 = 6
+var b = 5 
+var b2 = 5
+var resultado = ((a + a2) + (b + b2))
+
